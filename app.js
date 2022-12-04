ae7781729f4b523f1992bf11b80a4a87149acf71
Bayanduuren <bayakabaynduuren@gmail.com>
2022-12-04 19:05:06 +0800

    Yes All Done

--- app.js
+++ app.js
@@ -54,6 +54,22 @@ var uiController = (function () {
         today.getMonth() +
         " сарын Өрхийн санхүү";
     },
+
+    changeType: function () {
+      var fields = document.querySelectorAll(
+        DOMstrings.inputType +
+          ", " +
+          DOMstrings.inputDescription +
+          ", " +
+          DOMstrings.inputValue
+      );
+
+      nodeListForeach(fields, function (el) {
+        el.classList.toggle("red-focus");
+      });
+
+      document.querySelector(DOMstrings.addBtn).classList.toggle("red");
+    },
     getInput: function () {
       return {
         type: document.querySelector(DOMstrings.inputType).value, // exp, inc
@@ -322,9 +338,14 @@ var appController = (function (uiController, financeController) {
     });
 
     document
+      .querySelector(DOM.inputType)
+      .addEventListener("change", uiController.changeType);
+
+    document
       .querySelector(DOM.containerDiv)
       .addEventListener("click", function (event) {
         var id = event.target.parentNode.parentNode.parentNode.parentNode.id;
+
         if (id) {
           var arr = id.split("-");
           var type = arr[0];
